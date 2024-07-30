3db0ee03870a9d7a931f241f1fc217e8f78a9416
abdurrahman <arahmanmh15@gmail.com>
2024-07-30 15:23:07 +0800

    gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello world'),
-        ),
+        //backgroundColor: Color.fromRGBO(211, 106, 235, 1),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(211, 106, 235, 1),
+            Color.fromRGBO(106, 164, 235, 1)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world',
+          style: TextStyle(
+            color: Color.fromARGB(255, 8, 57, 0),
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
